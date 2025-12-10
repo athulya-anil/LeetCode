961ca87960a6522666adc36841ab45035f996633
Athulya Anil <athulyaanil@umass.edu>
2025-12-10 07:42:42 +0530

    Create 111-MinimumDepthOfBinaryTree

---
+++ 111-MinimumDepthOfBinaryTree/111-MinimumDepthOfBinaryTree.py
@@ -0,0 +1,26 @@
+# Last updated: 10/12/2025, 07:42:42
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution(object):
+    def minDepth(self, root):
+        """
+        :type root: Optional[TreeNode]
+        :rtype: int
+        """
+        if not root:
+            return(0)
+
+        if not root.left:
+            return (1 + self.minDepth(root.right))   
+        if not root.right:
+            return (1 + self.minDepth(root.left))         
+
+        l=self.minDepth(root.left)    
+        r=self.minDepth(root.right)   
+
+        return (1 + min(l,r))
+        

90d1d53f6827f6b021010dbf3a2f6e2aecc2d9f3
Athulya Anil <athulyaanil@umass.edu>
2025-12-10 07:39:42 +0530

    Create 3530-OddAndEvenTransactions

---
+++ 3530-OddAndEvenTransactions/3530-OddAndEvenTransactions.sql
@@ -0,0 +1,5 @@
+-- Last updated: 10/12/2025, 07:39:41
+# Write your MySQL query statement below
+
+select transaction_date, sum(case when amount%2 != 0 then amount else 0 end) as odd_sum, sum(case when amount%2 = 0 then amount else 0 end) as even_sum 
+from transactions group by 1 order by 1

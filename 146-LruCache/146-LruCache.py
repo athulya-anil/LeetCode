04c3689f8e50c7a1df87e3efec43bd4f2d840e50
Athulya Anil <athulyaanil@umass.edu>
2025-12-10 07:42:36 +0530

    Create 146-LruCache

---
+++ 146-LruCache/146-LruCache.py
@@ -0,0 +1,56 @@
+# Last updated: 10/12/2025, 07:42:36
+class Node:
+    def __init__(self, key, val):
+        self.key = key
+        self.val = val
+        self.prev = None
+        self.next = None
+
+class LRUCache(object):
+
+    def __init__(self, capacity):
+        self.cap = capacity
+        self.map = {}                    # key -> node
+
+        # dummy head & tail
+        self.head = Node(0, 0)
+        self.tail = Node(0, 0)
+        self.head.next = self.tail
+        self.tail.prev = self.head
+
+    # remove a node from list
+    def _remove(self, node):
+        p = node.prev
+        n = node.next
+        p.next = n
+        n.prev = p
+
+    # insert node right after head (most recent)
+    def _add(self, node):
+        node.next = self.head.next
+        node.prev = self.head
+        self.head.next.prev = node
+        self.head.next = node
+
+    def get(self, key):
+        if key not in self.map:
+            return -1
+
+        node = self.map[key]
+        self._remove(node)
+        self._add(node)           # move to front
+        return node.val
+
+    def put(self, key, value):
+        if key in self.map:
+            self._remove(self.map[key])
+
+        node = Node(key, value)
+        self._add(node)
+        self.map[key] = node
+
+        if len(self.map) > self.cap:
+            # remove LRU at tail.prev
+            lru = self.tail.prev
+            self._remove(lru)
+            del self.map[lru.key]

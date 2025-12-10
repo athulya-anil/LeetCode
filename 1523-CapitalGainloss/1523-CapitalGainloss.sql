5bccf5ad020c63004e2af68f56cfbf8f1d066959
Athulya Anil <athulyaanil@umass.edu>
2025-12-10 07:40:33 +0530

    Create 1523-CapitalGainloss

---
+++ 1523-CapitalGainloss/1523-CapitalGainloss.sql
@@ -0,0 +1,5 @@
+-- Last updated: 10/12/2025, 07:40:33
+/* Write your PL/SQL query statement below */
+SELECT stock_name, SUM(CASE WHEN operation = 'Buy' THEN -price ELSE price END) AS capital_gain_loss
+FROM Stocks
+GROUP BY stock_name;

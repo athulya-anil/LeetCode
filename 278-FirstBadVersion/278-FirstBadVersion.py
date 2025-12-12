f1611fc34550ff5cadbed57a95a0105a1c20ff6c
Athulya Anil <athulyaanil@umass.edu>
2025-12-12 09:22:59 +0530

    File updated at 12/12/2025, 09:22:59

--- 278-FirstBadVersion/278-FirstBadVersion.py
+++ 278-FirstBadVersion/278-FirstBadVersion.py
@@ -1,22 +1,25 @@
-# Last updated: 10/12/2025, 07:41:56
-# The isBadVersion API is already defined for you.
-# @param version, an integer
-# @return a bool
-# def isBadVersion(version):
-
-class Solution(object):
-    def firstBadVersion(self, n):
-        """
-        :type n: int
-        :rtype: int
-        """
-        beg=1
-        end=n
-        while beg<end:
-            mid=(beg+end)//2
-            if isBadVersion(mid) != True: # not bad
-                beg=mid+1
-            else:
-                end=mid    
-        return(beg)        
-        
+# Last updated: 12/12/2025, 09:22:59
+1# The isBadVersion API is already defined for you.
+2# @param version, an integer
+3# @return a bool
+4# def isBadVersion(version):
+5
+6class Solution(object):
+7    def firstBadVersion(self, n):
+8        """
+9        :type n: int
+10        :rtype: int
+11        """
+12        beg=1
+13        end=n
+14        ans=-1
+15        while beg<=end:
+16            mid=(beg+end)//2
+17            if isBadVersion(mid)==False:
+18                beg=mid+1
+19            elif isBadVersion(mid)==True:   
+20                ans=mid
+21                end=mid-1
+22        return(ans)         
+23
+24        

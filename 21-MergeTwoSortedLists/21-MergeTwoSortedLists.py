5136e7491c3fcd5c1eb8fd6e0335ecbbb23d3f1a
Athulya Anil <athulyaanil@umass.edu>
2025-12-16 19:33:03 +0530

    Create 21-MergeTwoSortedLists

---
+++ 21-MergeTwoSortedLists/21-MergeTwoSortedLists.py
@@ -0,0 +1,28 @@
+# Last updated: 16/12/2025, 19:33:02
+1# Definition for singly-linked list.
+2# class ListNode(object):
+3#     def __init__(self, val=0, next=None):
+4#         self.val = val
+5#         self.next = next
+6class Solution(object):
+7    def mergeTwoLists(self, list1, list2):
+8        """
+9        :type list1: Optional[ListNode]
+10        :type list2: Optional[ListNode]
+11        :rtype: Optional[ListNode]
+12        """
+13        dummy=ListNode()
+14        tail=dummy
+15        while list1 and list2:
+16            if list1.val<list2.val:
+17                tail.next= list1
+18                list1=list1.next
+19            else:
+20                tail.next=list2
+21                list2=list2.next
+22            tail=tail.next    
+23        if list1:
+24            tail.next=list1       
+25        else:
+26            tail.next=list2
+27        return (dummy.next)

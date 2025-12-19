4077b516a1ca938885239c18c26c63e65c5defd2
Athulya Anil <athulyaanil@umass.edu>
2025-12-19 19:04:39 +0530

    Create 104-MaximumDepthofBinaryTree

---
+++ 104-MaximumDepthofBinaryTree/104-MaximumDepthofBinaryTree.py
@@ -0,0 +1,23 @@
+# Last updated: 19/12/2025, 19:04:39
+1# Definition for a binary tree node.
+2# class TreeNode(object):
+3#     def __init__(self, val=0, left=None, right=None):
+4#         self.val = val
+5#         self.left = left
+6#         self.right = right
+7class Solution(object):
+8    def maxDepth(self, root):
+9        """
+10        :type root: Optional[TreeNode]
+11        :rtype: int
+12        """
+13        if not root:
+14            return 0
+15        left=self.maxDepth(root.left)   
+16        right=self.maxDepth(root.right)  
+17
+18        return 1+max(left,right)
+19    
+20    
+21
+22    

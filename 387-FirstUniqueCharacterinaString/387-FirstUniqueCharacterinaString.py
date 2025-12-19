edab211d2c335dfa70666953134d290ab58d9fb4
Athulya Anil <athulyaanil@umass.edu>
2025-12-19 17:08:34 +0530

    Create 387-FirstUniqueCharacterinaString

---
+++ 387-FirstUniqueCharacterinaString/387-FirstUniqueCharacterinaString.py
@@ -0,0 +1,19 @@
+# Last updated: 19/12/2025, 17:08:33
+1class Solution(object):
+2    def firstUniqChar(self, s):
+3        """
+4        :type s: str
+5        :rtype: int
+6        """
+7        count = defaultdict(int)
+8
+9        for char in s:
+10            count[char] += 1
+11
+12        for i,c in enumerate(s):
+13            if count[c] == 1:
+14                return i
+15                
+16        return -1        
+17
+18        

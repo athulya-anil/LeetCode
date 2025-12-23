c15dc318b552f16804537e207103011dd942a052
Athulya Anil <athulyaanil@umass.edu>
2025-12-23 23:07:52 +0530

    File updated at 23/12/2025, 23:07:52

--- 112-PathSum/112-PathSum.py
+++ 112-PathSum/112-PathSum.py
@@ -1,4 +1,4 @@
-# Last updated: 22/12/2025, 20:12:53
+# Last updated: 23/12/2025, 23:07:52
 1# Definition for a binary tree node.
 2# class TreeNode(object):
 3#     def __init__(self, val=0, left=None, right=None):
@@ -15,11 +15,11 @@
 14        def dfs(node, currSum):
 15            if not node:
 16                return False
-17                
-18            currSum += node.val
+17
+18            currSum+=node.val
 19            if not node.left and not node.right:
 20                return (currSum == targetSum)    
 21
-22            return (dfs(node.left,currSum) or dfs(node.right,currSum)) 
-23        
-24        return dfs(root,0)
+22            return (dfs(node.left, currSum)  or dfs(node.right, currSum))
+23        return dfs(root, 0)    
+24        

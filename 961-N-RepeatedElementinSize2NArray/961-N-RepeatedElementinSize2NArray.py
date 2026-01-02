6ef615a81357870c69418d8dfa8502b4798996aa
Athulya Anil <athulyaanil@umass.edu>
2026-01-02 22:26:16 +0530

    File updated at 02/01/2026, 22:26:16

--- 961-N-RepeatedElementinSize2NArray/961-N-RepeatedElementinSize2NArray.py
+++ 961-N-RepeatedElementinSize2NArray/961-N-RepeatedElementinSize2NArray.py
@@ -1,4 +1,4 @@
-# Last updated: 02/01/2026, 22:24:33
+# Last updated: 02/01/2026, 22:26:16
 1class Solution(object):
 2    def repeatedNTimes(self, nums):
 3        """
@@ -7,17 +7,12 @@
 6        """
 7        seen=[]
 8        repeated=[]
-9        length=len(nums)  
-10        n=length/2
-11        unique_elements=n+1
-12        repeated_elements=length-unique_elements
-13        for i in nums:
-14            if i not in seen:
-15                seen.append(i)
-16            else:
-17                repeated.append(i)    
-18                
-19        return(repeated[0])        
-20
-21
-22
+9        for i in nums:
+10            if i not in seen:
+11                seen.append(i)
+12            else:
+13                repeated.append(i)                
+14        return(repeated[0])        
+15
+16
+17

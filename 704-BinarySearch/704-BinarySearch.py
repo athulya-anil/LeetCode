a82945e353f9dd56ee7765c835c3b4435396449e
Athulya Anil <athulyaanil@umass.edu>
2026-01-05 22:38:53 +0530

    Create 704-BinarySearch

---
+++ 704-BinarySearch/704-BinarySearch.py
@@ -0,0 +1,20 @@
+# Last updated: 05/01/2026, 22:38:52
+1class Solution(object):
+2    def search(self, nums, target):
+3        """
+4        :type nums: List[int]
+5        :type target: int
+6        :rtype: int
+7        """
+8        beg=0
+9        end=len(nums)-1
+10        while beg<=end:
+11            mid=(beg+end)//2
+12            if nums[mid]==target:
+13                return(mid)
+14            elif nums[mid]<target:
+15                beg=mid+1
+16            else:
+17                end=mid-1
+18        return (-1)                
+19        

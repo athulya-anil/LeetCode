c394c253bab727c16bf1b43de8b3016f96856784
Athulya Anil <athulyaanil@umass.edu>
2026-02-03 17:50:36 -0500

    File updated at 03/02/2026, 17:50:36

--- 283-MoveZeroes/283-MoveZeroes.py
+++ 283-MoveZeroes/283-MoveZeroes.py
@@ -1,16 +1,13 @@
-# Last updated: 03/02/2026, 17:49:39
+# Last updated: 03/02/2026, 17:50:36
 1class Solution(object):
 2    def moveZeroes(self, nums):
 3        """
 4        :type nums: List[int]
 5        :rtype: None Do not return anything, modify nums in-place instead.
 6        """
-7        slow=0
-8        fast=0
-9
-10        while fast<len(nums):
-11            if nums[fast]!=0:
-12                nums[slow],nums[fast]=nums[fast],nums[slow]
-13                slow+=1
-14            fast+=1    
-15        
+7        f=s=0
+8        while f<len(nums):
+9            if nums[f]!=0:
+10                nums[f],nums[s]=nums[s],nums[f]
+11                s+=1
+12            f+=1    

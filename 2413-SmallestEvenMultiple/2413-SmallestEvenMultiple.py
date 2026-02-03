6a51c01717357eafa31ff741e7ebe78ccbf08c26
Athulya Anil <athulyaanil@umass.edu>
2026-02-03 17:12:25 -0500

    Create 2413-SmallestEvenMultiple

---
+++ 2413-SmallestEvenMultiple/2413-SmallestEvenMultiple.py
@@ -0,0 +1,8 @@
+# Last updated: 03/02/2026, 17:12:25
+1class Solution(object):
+2    def smallestEvenMultiple(self, n):
+3        if n % 2 == 0:
+4            return (n)
+5        else:
+6            return(n*2)    
+7        

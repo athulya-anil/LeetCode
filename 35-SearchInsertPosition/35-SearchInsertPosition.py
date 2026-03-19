7e597b9af2acee12b2be927a6067579552149fe0
Athulya Anil <athulyaanil@umass.edu>
2026-03-19 15:12:52 -0400

    File updated at 19/03/2026, 15:12:52

--- 35-SearchInsertPosition/35-SearchInsertPosition.py
+++ 35-SearchInsertPosition/35-SearchInsertPosition.py
@@ -1,16 +1,15 @@
-# Last updated: 05/01/2026, 22:45:27
+# Last updated: 19/03/2026, 15:12:52
 1class Solution(object):
 2    def searchInsert(self, nums, target):
 3        beg=0
 4        end=len(nums)-1
 5        while beg<=end:
 6            mid=(beg+end)//2
-7            if nums[mid] == target:
+7            if nums[mid]==target:
 8                return(mid)
 9            elif nums[mid]>target:
 10                end=mid-1
 11            else:
-12                beg=mid+1
-13        return(beg)            
-14
-15        
+12                beg=mid+1  
+13        return(beg)              
+14        

59d177d3c24902ce5f896f40b49edd5fe1b47c4e
Athulya Anil <athulyaanil@umass.edu>
2026-03-25 15:22:18 -0400

    File updated at 25/03/2026, 15:22:18

--- 1046-LastStoneWeight/1046-LastStoneWeight.py
+++ 1046-LastStoneWeight/1046-LastStoneWeight.py
@@ -1,4 +1,4 @@
-# Last updated: 24/03/2026, 21:41:51
+# Last updated: 25/03/2026, 15:22:18
 1import heapq
 2
 3class Solution(object):
@@ -8,21 +8,22 @@
 7        :rtype: int
 8        """
 9        n=len(stones)
-10        for i in range (n):
+10        for i in range(n):
 11            stones[i]=-stones[i]
 12
-13        heapq.heapify(stones)    
-14
-15        while len(stones) > 1:
-16
-17            largest=heapq.heappop(stones)
-18            second_largest=heapq.heappop(stones)
+13        heapq.heapify(stones)
+14        while len(stones)>1:
+15            largest=heapq.heappop(stones)
+16            second_largest=heapq.heappop(stones)
+17            if second_largest != largest:
+18                heapq.heappush(stones,largest-second_largest)
 19
-20            if largest != second_largest:
-21                heapq.heappush(stones,largest - second_largest)
-22
-23        if len(stones)==1:
-24            return(-stones[0])
-25        else:
-26            return 0        
-27        
+20        if len(stones) == 1:
+21            return(-heapq.heappop(stones))
+22        else:
+23            return 0        
+24
+25
+26
+27
+28        

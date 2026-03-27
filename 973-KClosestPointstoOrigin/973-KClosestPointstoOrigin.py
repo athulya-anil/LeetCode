c33e7de2a0fade0d684f4b6079afb501fee5b3a8
Athulya Anil <athulyaanil@umass.edu>
2026-03-27 14:50:16 -0400

    File updated at 27/03/2026, 14:50:16

--- 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.py
+++ 973-KClosestPointstoOrigin/973-KClosestPointstoOrigin.py
@@ -1,4 +1,4 @@
-# Last updated: 27/03/2026, 00:08:07
+# Last updated: 27/03/2026, 14:50:16
 1import heapq
 2class Solution(object):
 3    def kClosest(self, points, k):
@@ -9,16 +9,14 @@
 8        """
 9        def dist(x,y):
 10            return(x**2+y**2)
-11        ls=[]
+11        heap=[]
 12        for x,y in points:
 13            d=dist(x,y)
-14            if len(ls) < k:
-15                heapq.heappush(ls,(-d,x,y))
-16            else:
-17                heapq.heappushpop(ls,(-d,x,y))
-18
-19        return [[x,y] for d,x,y in ls]           
+14        
+15
+16            if len(heap) < k:
+17                heapq.heappush(heap,(-d,x,y))
+18            else:
+19                heapq.heappushpop(heap,(-d,x,y))    
 20
-21
-22
-23        
+21        return[[x,y] for d,x,y in heap]    

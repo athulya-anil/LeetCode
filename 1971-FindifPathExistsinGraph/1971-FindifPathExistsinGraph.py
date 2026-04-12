c9c6114fa6ef659a00a493ece545cdb53545362a
Athulya Anil <athulyaanil@umass.edu>
2026-04-12 12:48:07 -0400

    File updated at 12/04/2026, 12:48:07

--- 1971-FindifPathExistsinGraph/1971-FindifPathExistsinGraph.py
+++ 1971-FindifPathExistsinGraph/1971-FindifPathExistsinGraph.py
@@ -1,4 +1,4 @@
-# Last updated: 11/04/2026, 16:52:01
+# Last updated: 12/04/2026, 12:48:07
 1from collections import defaultdict
 2
 3class Solution(object):
@@ -11,11 +11,11 @@
 10        :rtype: bool
 11        """
 12        if source == destination:
-13                return True
+13            return True
 14
-15        seen=set()
-16        seen.add(source)
-17        graph=defaultdict(list)
+15        graph=defaultdict(list)
+16        seen=set()
+17        seen.add(source)
 18
 19        for u,v in edges:
 20            graph[u].append(v)
@@ -24,13 +24,15 @@
 23        def dfs(i):
 24            if i == destination:
 25                return True
-26            for neigh in graph[i]:
-27                if neigh not in seen:
-28                    seen.add(neigh)
-29                    if dfs(neigh):
-30                        return True
-31            return(False) 
-32        return dfs(source)               
-33
-34
+26
+27            for neigh in graph[i]:
+28                if neigh not in seen:
+29                    seen.add(neigh)
+30                    if dfs(neigh):
+31                        return True
+32            return(False)   
+33        return dfs(source)            
+34                
 35
+36
+37

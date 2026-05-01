90e074bb5584e05deb7c78c9604b9fc860fa069d
Athulya Anil <athulyaanil@umass.edu>
2026-05-01 08:29:26 -0400

    File updated at 01/05/2026, 08:29:26

--- 200-NumberofIslands/200-NumberofIslands.py
+++ 200-NumberofIslands/200-NumberofIslands.py
@@ -1,28 +1,28 @@
-# Last updated: 15/04/2026, 06:13:04
+# Last updated: 01/05/2026, 08:29:26
 1class Solution(object):
 2    def numIslands(self, grid):
 3        """
 4        :type grid: List[List[str]]
 5        :rtype: int
 6        """
-7        island=0
-8        u=len(grid)
-9        v=len(grid[0])
-10        def dfs(i,j):
-11            if i<0 or j<0 or i>=u or j>=v or grid[i][j]!="1":
-12                return 0
-13            else:
-14                grid[i][j]="0"
-15                dfs(i,j-1)
-16                dfs(i,j+1)
-17                dfs(i-1,j)
-18                dfs(i+1,j)    
-19
-20        for i in range (u):
-21            for j in range(v):
-22                if grid[i][j]== "1":
-23                    island+=1
-24                    dfs(i,j)
-25
-26        return (island)
-27        
+7        u=len(grid)
+8        v=len(grid[0])
+9        island=0
+10
+11        def dfs(i,j):
+12            if i<0 or j<0 or i>=u or j>=v or grid[i][j]=="0":
+13                return 0
+14            else:
+15                grid[i][j]="0"
+16                dfs(i,j-1)
+17                dfs(i,j+1)
+18                dfs(i-1,j)
+19                dfs(i+1,j)    
+20
+21        for i in range(u):
+22            for j in range(v):
+23                if grid[i][j]=="1":
+24                    island+=1
+25                    dfs(i,j)
+26
+27        return(island)

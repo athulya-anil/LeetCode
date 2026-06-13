aa9de3063f447a7ad00576ed68177b28bbd26f9a
Athulya Anil <athulyaanil@umass.edu>
2026-06-13 18:10:54 -0400

    File updated at 13/06/2026, 18:10:54

--- 1581-CustomerWhoVisitedbutDidNotMakeAnyTransactions/1581-CustomerWhoVisitedbutDidNotMakeAnyTransactions.py
+++ 1581-CustomerWhoVisitedbutDidNotMakeAnyTransactions/1581-CustomerWhoVisitedbutDidNotMakeAnyTransactions.py
@@ -1,4 +1,4 @@
-# Last updated: 10/03/2026, 15:53:03
+# Last updated: 13/06/2026, 18:10:54
 df = visits_no_trans.groupby('customer_id', as_index=False)['visit_id'].count()
 
 return df.rename(columns={'visit_id': 'count_no_trans'})

ad93b75cdba9deebab4d93ca64164131c3609c57
Athulya Anil <athulyaanil@umass.edu>
2026-06-14 23:02:46 -0400

    Create 1013-FibonacciNumber

---
+++ 1013-FibonacciNumber/1013-FibonacciNumber.py
@@ -0,0 +1,12 @@
+# Last updated: 14/06/2026, 23:02:45
+class Solution(object):
+    def fib(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        if n == 0:
+            return 0
+        if n == 1:
+            return 1
+        return self.fib(n-1) + self.fib(n-2)        

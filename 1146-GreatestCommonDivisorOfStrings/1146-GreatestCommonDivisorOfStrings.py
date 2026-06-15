1a779583f9711d99d1805136283c1eb5adc70e36
Athulya Anil <athulyaanil@umass.edu>
2026-06-14 23:02:35 -0400

    Create 1146-GreatestCommonDivisorOfStrings

---
+++ 1146-GreatestCommonDivisorOfStrings/1146-GreatestCommonDivisorOfStrings.py
@@ -0,0 +1,19 @@
+# Last updated: 14/06/2026, 23:02:34
+class Solution(object):
+    def gcdOfStrings(self, str1, str2):
+        """
+        :type str1: str
+        :type str2: str
+        :rtype: str
+        """
+        if str1+str2!=str2+str1:
+            return ""
+        def gcd(a, b):
+            while b:
+                a, b = b, a % b
+            return a
+        length = gcd(len(str1),len(str2))    
+        return(str1[0:length])
+    
+        
+        

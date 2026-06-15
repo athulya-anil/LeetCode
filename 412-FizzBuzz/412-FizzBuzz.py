56488d19cd0d5643bc84d7f4bb1de3856c4b1e9f
Athulya Anil <athulyaanil@umass.edu>
2026-06-14 23:03:22 -0400

    Create 412-FizzBuzz

---
+++ 412-FizzBuzz/412-FizzBuzz.py
@@ -0,0 +1,18 @@
+# Last updated: 14/06/2026, 23:03:21
+class Solution(object):
+    def fizzBuzz(self, n):
+        """
+        :type n: int
+        :rtype: List[str]
+        """
+        result=[]
+        for i in range (1,n+1):
+            if (i%3==0 and i%5==0):
+                result.append('FizzBuzz')
+            elif(i%3==0):   
+                result.append('Fizz')
+            elif(i%5==0):    
+                result.append('Buzz') 
+            else:
+                result.append(str(i))   
+        return(result)

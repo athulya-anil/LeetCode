4f978e6b43c96b4abe70b250acde36a645d09577
Athulya Anil <athulyaanil@umass.edu>
2026-07-10 12:00:57 -0400

    Create 33-SearchInRotatedSortedArray

---
+++ 33-SearchInRotatedSortedArray/33-SearchInRotatedSortedArray.py
@@ -0,0 +1,32 @@
+# Last updated: 10/07/2026, 12:00:57
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        beg=0
+        end=len(nums)-1
+        while beg<=end:
+            mid=(beg+end)//2
+            if nums[mid]==target:
+                    return(mid)
+            if nums[beg] <= nums[mid]: #left half
+                if nums[beg]<=target<nums[mid]:
+                    end=mid-1
+                else:
+                    beg=mid+1    
+            else: #right half
+                if nums[mid]<target<=nums[end]:
+                    beg=mid+1 
+                else:
+                    end=mid-1   
+        return(-1)             
+
+
+
+
+        
+
+        

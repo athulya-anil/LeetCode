426a3f293bee0b33455acb189127f1a37dc558ff
Athulya Anil <athulyaanil@umass.edu>
2026-07-12 02:54:13 -0400

    Create 1735-TheMostFrequentlyOrderedProductsForEachCustomer

---
+++ 1735-TheMostFrequentlyOrderedProductsForEachCustomer/1735-TheMostFrequentlyOrderedProductsForEachCustomer.sql
@@ -0,0 +1,13 @@
+-- Last updated: 12/07/2026, 02:54:13
+# Write your MySQL query statement below
+with total_order_count as(
+    select customer_id, product_id, count(*) as order_count
+    from Orders
+    group by 1,2
+),
+ranked as (select customer_id, product_id,
+DENSE_RANK() OVER(PARTITION BY customer_id order by order_count desc) as rk
+from total_order_count)
+
+select r.customer_id, r.product_id, product_name from ranked r join Products p on p.product_id = r.product_id
+where rk = 1

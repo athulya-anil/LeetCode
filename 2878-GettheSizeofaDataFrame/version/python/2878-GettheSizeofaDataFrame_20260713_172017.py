becdd7fdc3b011b9a7c6e8356c35c101dc80959e
Athulya Anil <athulyaanil@umass.edu>
2026-07-13 17:20:17 -0400

    Create 2878-GettheSizeofaDataFrame

---
+++ 2878-GettheSizeofaDataFrame/version/python/2878-GettheSizeofaDataFrame_20260713_172017.py
@@ -0,0 +1,6 @@
+# Last updated: 13/07/2026, 17:20:17
+1import pandas as pd
+2
+3def selectData(students: pd.DataFrame) -> pd.DataFrame:
+4    return(students.loc[students['student_id']==101,['name','age']])
+5    

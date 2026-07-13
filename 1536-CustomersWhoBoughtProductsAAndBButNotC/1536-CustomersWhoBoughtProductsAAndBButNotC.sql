6caa89a39998d1dda12653f8297320cc102e8157
Athulya Anil <athulyaanil@umass.edu>
2026-07-13 16:47:53 -0400

    Create 1536-CustomersWhoBoughtProductsAAndBButNotC

---
+++ 1536-CustomersWhoBoughtProductsAAndBButNotC/1536-CustomersWhoBoughtProductsAAndBButNotC.sql
@@ -0,0 +1,9 @@
+-- Last updated: 13/07/2026, 16:47:53
+# Write your MySQL query statement below
+select c.customer_id, c.customer_name
+from Orders o left join Customers c
+on c.customer_id = o.customer_id
+group by 1,2
+having sum(product_name='A') >=1
+and sum(product_name='B') >=1
+and sum(product_name='C') = 0

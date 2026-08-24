db6a43febf13414324d0f95f9c14b119a7936f37
Athulya Anil <athulyaanil@umass.edu>
2026-08-24 12:56:04 -0400

    Create 603-ConsecutiveAvailableSeats

---
+++ 603-ConsecutiveAvailableSeats/603-ConsecutiveAvailableSeats.sql
@@ -0,0 +1,7 @@
+-- Last updated: 24/08/2026, 12:56:04
+# Write your MySQL query statement below
+select distinct c1.seat_id
+from Cinema c1 join Cinema c2
+on abs(c1.seat_id - c2.seat_id)=1
+where c1.free = 1 and c2.free = 1
+order by seat_id

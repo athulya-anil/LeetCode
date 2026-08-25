4e2ae6baada7d9e39df2247c36cd392ed90b5ac2
Athulya Anil <athulyaanil@umass.edu>
2026-08-25 13:02:04 -0400

    Create 162-FindPeakElement

---
+++ 162-FindPeakElement/version/python/162-FindPeakElement_20260825_130203.py
@@ -0,0 +1,16 @@
+# Last updated: 25/08/2026, 13:02:03
+1class Solution(object):
+2    def findPeakElement(self, nums):
+3        """
+4        :type nums: List[int]
+5        :rtype: int
+6        """
+7        beg=0
+8        end=len(nums)-1
+9        while beg<end:
+10            mid=(beg+end)//2
+11            if nums[mid]<nums[mid+1]:
+12                beg=mid+1
+13            else:
+14                end=mid    
+15        return(end)   

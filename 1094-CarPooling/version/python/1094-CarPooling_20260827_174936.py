aa6ec17d8829008c5cba76c05c8e9ad5812f44b9
Athulya Anil <athulyaanil@umass.edu>
2026-08-27 17:49:37 -0400

    Create 1094-CarPooling

---
+++ 1094-CarPooling/version/python/1094-CarPooling_20260827_174936.py
@@ -0,0 +1,29 @@
+# Last updated: 27/08/2026, 17:49:36
+1class Solution(object):
+2    def carPooling(self, trips, capacity):
+3        """
+4        :type trips: List[List[int]]
+5        :type capacity: int
+6        :rtype: bool
+7        """
+8        curr_passenger_count=0
+9        passenger_ride = [0]*1001
+10        new_passenger_ride = [0]*1001
+11
+12        for passengers, start_destination, end_destination in trips:
+13            passenger_ride[start_destination]+=passengers
+14            passenger_ride[end_destination]-=passengers
+15
+16        for i in range(len(passenger_ride)):
+17            curr_passenger_count+=passenger_ride[i]
+18            new_passenger_ride[i]=curr_passenger_count 
+19
+20        if capacity >= (max(new_passenger_ride)):
+21            return True
+22        else:
+23            return False        
+24        
+25
+26
+27
+28        

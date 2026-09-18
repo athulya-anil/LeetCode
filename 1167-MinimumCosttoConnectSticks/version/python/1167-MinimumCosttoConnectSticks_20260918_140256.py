0e4385b55fd6233d1b9aad1fe769a794f6c5b23e
Athulya Anil <athulyaanil@umass.edu>
2026-09-18 14:02:56 -0400

    Create 1167-MinimumCosttoConnectSticks

---
+++ 1167-MinimumCosttoConnectSticks/version/python/1167-MinimumCosttoConnectSticks_20260918_140256.py
@@ -0,0 +1,13 @@
+# Last updated: 18/09/2026, 14:02:56
+1class Solution:
+2    def connectSticks(self, sticks: list[int]) -> int:
+3        total_cost=0
+4        while len(sticks)>1:
+5            sticks.sort()
+6            cost=sticks[0]+sticks[1]
+7            sticks=sticks[2:]
+8            sticks.append(cost)
+9            total_cost+=cost
+10        return total_cost    
+11
+12        

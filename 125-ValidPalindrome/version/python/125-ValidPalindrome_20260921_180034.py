a9b7ff5949f50dbf6638b24afcf261bbdbb2e4a0
Athulya Anil <athulyaanil@umass.edu>
2026-09-21 18:00:34 -0400

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/version/python/125-ValidPalindrome_20260921_180034.py
@@ -0,0 +1,15 @@
+# Last updated: 21/09/2026, 18:00:34
+1class Solution:
+2    def isPalindrome(self, s: str) -> bool:
+3        s=s.lower()
+4        s=''.join (c for c in s if c.isalnum())
+5        l=0
+6        r=len(s)-1
+7        while l<r:
+8            if s[l]!=s[r]:
+9                return False
+10            else:    
+11                l=l+1
+12                r=r-1
+13        return True    
+14        

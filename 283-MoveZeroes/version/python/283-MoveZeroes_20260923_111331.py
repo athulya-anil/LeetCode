474bbfeca5885d9af092801ae10e796ad9dcd6f1
Athulya Anil <athulyaanil@umass.edu>
2026-09-23 11:13:31 -0400

    Create 283-MoveZeroes

---
+++ 283-MoveZeroes/version/python/283-MoveZeroes_20260923_111331.py
@@ -0,0 +1,13 @@
+# Last updated: 23/09/2026, 11:13:31
+1class Solution:
+2    def moveZeroes(self, nums: list[int]) -> None:
+3        """
+4        Do not return anything, modify nums in-place instead.
+5        """
+6        slow=fast=0
+7        while fast<len(nums):
+8            if nums[fast]!=0:
+9                nums[slow],nums[fast]=nums[fast],nums[slow]
+10                slow+=1
+11            fast+=1
+12        return nums        

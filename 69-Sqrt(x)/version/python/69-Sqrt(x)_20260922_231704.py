b1b3ecceefed2bd830e860bcfef76ce8839509e1
Athulya Anil <athulyaanil@umass.edu>
2026-09-22 23:17:04 -0400

    Create 69-Sqrt(x)

---
+++ 69-Sqrt(x)/version/python/69-Sqrt(x)_20260922_231704.py
@@ -0,0 +1,16 @@
+# Last updated: 22/09/2026, 23:17:04
+1class Solution:
+2    def mySqrt(self, x: int) -> int:
+3        beg=1
+4        end=x
+5        while beg<=end:
+6            mid=(beg+end)//2
+7            sq=mid*mid
+8            if sq == x:
+9                return mid
+10            elif sq<x:
+11                beg=mid+1
+12            else:
+13                end=mid-1
+14        return end                 
+15
